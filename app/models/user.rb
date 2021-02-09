74e07f4e2a09d6ef97a85b907277c845c021cb48
Rene Wang <rene.wang1@hotmail.com>
2021-02-09 02:52:18 +0000

    add strip and downcase to authenticate_with_credentials for edge cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,15 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: 3}
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.where('lower(email) = ?', email.strip.downcase).first
+    # @user = User.find_by(email: email.strip)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
   
 end

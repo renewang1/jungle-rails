e6954decb59c44de7a25606edb046035536e6731
Rene Wang <rene.wang1@hotmail.com>
2021-02-09 02:03:51 +0000

    add validation to check if email exists in db and if password is too short

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 3}
   validates :password_confirmation, presence: true
   
 end

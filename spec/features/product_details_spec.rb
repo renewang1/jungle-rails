9751940d26579f0c57ae3ef84842a8ed27104555
Rene Wang <rene.wang1@hotmail.com>
2021-02-10 01:16:44 +0000

    add test case for clicking on detail button

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
+RSpec.feature "ProductDetails", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -17,16 +16,17 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-
   scenario "They see all products" do
     # ACT
-    visit root_path
+    visit products_path
+    save_screenshot 'product_page.png'
+
+    click_link('Details', match: :first)
 
     # DEBUG / VERIFY
-    save_screenshot 'home_page.png'
+    save_screenshot 'product_details.png'
 
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css 'article.product-detail'
 
   end
-
 end

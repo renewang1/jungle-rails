9acbf03cab7e632433b5e7245a2cf2e95563c20d
Rene Wang <rene.wang1@hotmail.com>
2021-02-10 02:00:25 +0000

    add capybara commands to click add button

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature, js: true do
+RSpec.feature "Add to cart", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -15,17 +15,17 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
       )
     end
   end
-
   scenario "They see all products" do
     # ACT
     visit products_path
+    save_screenshot 'product_page.png'
 
-    click_button('Add', match: :first)
+    click_link('Details', match: :first)
 
     # DEBUG / VERIFY
-    save_screenshot 'add_to_cart.png'
+    save_screenshot 'product_details.png'
 
-    expect(page).to have_text 'My Cart (1)'
+    expect(page).to have_css 'article.product-detail'
 
   end
 end
